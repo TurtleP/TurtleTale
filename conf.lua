64529982d37ff21b9c1fd2ed2e260cc6d785de66
TurtleP <jpostelnek@outlook.com>
2020-09-11 20:53:24 -0400

    restart

--- conf.lua
+++ conf.lua
@@ -1,16 +1,3 @@
 function love.conf(t)
-	t.console = false
-	t.identity = "TurtleTale"
-
-	if love._os then
-		t.window.width = 400
-		t.window.height = 480
-
-		t.window.title = "Turtle Tale"
-		t.window.highdpi = true
-		t.window.icon = "graphics/icon.png"
-
-		t.window.vsync = true
-		t.window.fullscreentype = "exclusive"
-	end
+    t.console = true
 end
--- data/scenes/inmountain.lua
+++
@@ -1,8 +0,0 @@
-return
-{
-	{"levelequals", "mountain_1-1"},
-	{"freezeplayer"},
-	{"dialog", {"turtle", "Woah. This .. place looks spooky. I'd better be careful.."}},
-	{"sleep", 0.2},
-	{"unfreezeplayer"}
-}
